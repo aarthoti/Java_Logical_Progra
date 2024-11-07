c75e8ba904c8c4e34de8d6cc762abf5ee849341d
aarthoti <165478144+aarthoti@users.noreply.github.com>
2024-11-07 16:14:51 +0530

    Add files via upload

---
+++ Char.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Char{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+char ch='e';
+if(ch =='a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+{
+System.out.println("OVL");
+}
+else
+{
+System.out.println("CON");
+}
+}
+}
